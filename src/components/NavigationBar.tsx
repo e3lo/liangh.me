cb0026b2b7d6c50d8e0719d7b877fdf75c6345b9
Leo Heng <leoheng905@gmail.com>
2023-09-23 14:40:51 +1000

    Added colors for nav to indicate acitve page

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,34 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const NavigationBar = () => {
+  const location = useLocation();
+
+  // Makes the selected page in nav green
+  const linkStying = (title: String) => {
+    console.log(location.pathname);
+    return (
+      "font-medium transition " +
+      (location.pathname === title ? "text-green-400" : "hover:text-green-200")
+    );
+  };
+
   return (
     <nav className="w-screen flex justify-center">
       <div className="flex max-w-6xl w-full justify-between mx-32 my-6 p-1">
         <div>
-          <Link
-            to={"/"}
-            className="font-medium underline hover:text-green-300 transition"
-          >
+          <Link to={"/"} className={linkStying("/") + " underline"}>
             Liang Heng
           </Link>
         </div>
         <div className="flex gap-6">
-          <Link
-            to={"/Achievements"}
-            className="font-medium hover:text-green-300 transition"
-          >
+          <Link to={"/Achievements"} className={linkStying("/Achievements")}>
             Achievements
           </Link>
-          <Link
-            to={`/Projects`}
-            className="font-medium hover:text-green-300 transition"
-          >
+          <Link to={`/Projects`} className={linkStying("/Projects")}>
             Projects
           </Link>
-          <Link
-            to={`/Contact`}
-            className="font-medium hover:text-green-300 transition"
-          >
+          <Link to={`/Contact`} className={linkStying("/Contact")}>
             Contact
           </Link>
         </div>
