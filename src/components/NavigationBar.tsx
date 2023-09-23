301adc2b20fc0bcc24b9976a82a5a4a570191def
Leo Heng <leoheng905@gmail.com>
2023-09-23 14:24:08 +1000

    Changed style to dark theme

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -5,14 +5,32 @@ const NavigationBar = () => {
     <nav className="w-screen flex justify-center">
       <div className="flex max-w-6xl w-full justify-between mx-32 my-6 p-1">
         <div>
-          <Link to={"/"} className="underline">
+          <Link
+            to={"/"}
+            className="font-medium underline hover:text-green-300 transition"
+          >
             Liang Heng
           </Link>
         </div>
-        <div className="flex gap-4">
-          <Link to={"/Achievements"}>Achievements</Link>
-          <Link to={`/Projects`}>Projects</Link>
-          <Link to={`/Contact`}>Contact</Link>
+        <div className="flex gap-6">
+          <Link
+            to={"/Achievements"}
+            className="font-medium hover:text-green-300 transition"
+          >
+            Achievements
+          </Link>
+          <Link
+            to={`/Projects`}
+            className="font-medium hover:text-green-300 transition"
+          >
+            Projects
+          </Link>
+          <Link
+            to={`/Contact`}
+            className="font-medium hover:text-green-300 transition"
+          >
+            Contact
+          </Link>
         </div>
       </div>
     </nav>
