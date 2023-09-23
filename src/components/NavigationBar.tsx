94d29cfcfe4518784e9a2d8a23d38be4a4ab0bd8
Leo Heng <leoheng905@gmail.com>
2023-09-23 20:59:03 +1000

    Made navigation bar responsive

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 const NavigationBar = () => {
@@ -12,24 +13,55 @@ const NavigationBar = () => {
     );
   };
 
+  const [mobileMenu, setMobileMenu] = useState(false);
+
   return (
-    <nav className="w-full flex justify-center left-0">
-      <div className="flex max-w-6xl w-full justify-between mx-32 my-6 py-1">
-        <div>
+    <nav className="w-full flex justify-center">
+      <div className="flex flex-wrap max-w-6xl w-full justify-between items-center mx-16 md:mx-32 py-4">
+        <div className="">
           <Link to={"/"} className={linkStying("/") + " underline"}>
             Liang Heng
           </Link>
         </div>
-        <div className="flex gap-6">
-          <Link to={"/Achievements"} className={linkStying("/Achievements")}>
-            Achievements
-          </Link>
-          <Link to={`/Projects`} className={linkStying("/Projects")}>
-            Projects
-          </Link>
-          <Link to={`/Contact`} className={linkStying("/Contact")}>
-            Contact
-          </Link>
+        <button
+          className="md:hidden p-2 w-10 h-10 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-600"
+          onClick={() => {
+            setMobileMenu(!mobileMenu);
+          }}
+        >
+          <svg
+            className="w-6 h-6 text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
+        </button>
+        <div
+          className={
+            (!mobileMenu && "hidden ") +
+            " w-full text-center md:flex md:w-auto gap-6"
+          }
+        >
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-zinc-700 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent">
+            <Link to={"/Achievements"} className={linkStying("/Achievements")}>
+              <li className="p-2">Achievements</li>
+            </Link>
+            <Link to={`/Projects`} className={linkStying("/Projects")}>
+              <li className="p-2">Projects</li>
+            </Link>
+            <Link to={`/Contact`} className={linkStying("/Contact")}>
+              <li className="p-2">Contact</li>
+            </Link>
+          </ul>
         </div>
       </div>
     </nav>
