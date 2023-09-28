f6e2d0374dfcca4676eb135527e7bd507f7233c9
Leo Heng <leoheng905@gmail.com>
2023-09-28 23:32:05 +1000

    Animation for hero header

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -43,24 +43,40 @@ const HeroBanner = () => {
       </div>
       {/* Foreground grid */}
       <div className="grid items-center grid-cols-6 grid-rows-3 gap-1 w-4/5 h-3/4 md:h-1/2 lg:h-3/4 absolute">
-        <div className="col-start-2 md:col-start-1 row-start1 md:row-start-2 col-span-4 md:col-span-1 row-span-1">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          className="col-start-2 md:col-start-1 row-start1 md:row-start-2 col-span-4 md:col-span-1 row-span-1"
+        >
           <img
             src={profilePic}
             className=" rounded-md max-w-full max-h-full shadow-md p-4"
           ></img>
-        </div>
+        </motion.div>
         {/* Emoji Thing */}
-        <div className="hidden md:block col-start-2 row-start-2 col-span-1 justify-self-center row-span-1 text-6xl">
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          className="hidden md:block col-start-2 row-start-2 col-span-1 justify-self-center row-span-1 text-6xl"
+        >
           📌
-        </div>
-        <div className=" col-start-2 row-start-2 col-span-4 md:col-start-3 justify-self-center md:justify-self-start text-6xl text-center md:text-[9vw] font-bold">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          className=" col-start-2 row-start-2 col-span-4 md:col-start-3 justify-self-center md:justify-self-start text-6xl text-center md:text-[9vw] font-bold"
+        >
           Liang Heng
-        </div>
-        <div className=" col-start-2 md:col-start-3 col-span-4 md:col-span-3 row-start-3 self-start max-w-md">
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, scale: 0.9 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          className=" col-start-2 md:col-start-3 col-span-4 md:col-span-3 row-start-3 self-start max-w-md"
+        >
           Hey! I'm a uni student focusing on front end web development and UI/UX
           design.
           <SocialBar xAlign={true}></SocialBar>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
