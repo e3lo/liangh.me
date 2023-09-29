56408055ea8bcdda336c6a791a13e4bbc8f3184f
Leo Heng <leoheng905@gmail.com>
2023-09-29 19:03:59 +1000

    Animations for comingsoon

--- src/components/ComingSoon.tsx
+++ src/components/ComingSoon.tsx
@@ -1,12 +1,19 @@
+import { motion } from "framer-motion";
+
 interface Props {
   children: string;
 }
 
 const ComingSoon = ({ children }: Props) => {
   return (
-    <div className="w-screen max-w-full h-screen flex items-center justify-center">
+    <motion.div
+      initial={{ opacity: 0, scale: 0.9 }}
+      whileInView={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+      className="w-screen max-w-full h-screen flex items-center justify-center"
+    >
       <div className="text-4xl max-w-lg">{children}</div>
-    </div>
+    </motion.div>
   );
 };
 
