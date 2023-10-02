422fcd16f4552ca9d263461ce994a6a04471adf4
Leo Heng <leoheng905@gmail.com>
2023-10-02 17:36:38 +1100

    Removed scroll hijacking
    Was too distracting and did not add any value

--- src/components/ProjectShowcase.tsx
+++ src/components/ProjectShowcase.tsx
@@ -7,7 +7,7 @@ const CardBanner = () => {
   return (
     <>
       <div className=" flex flex-col m-auto items-center max-w-sm text-center gap-2 mt-16 my-8">
-        <h1 className=" text-4xl font-bold">UI/UX</h1>
+        <h1 className=" text-4xl font-bold">Projects</h1>
         <p>
           Creating stunning interfaces that captures your attention is a good
           thing (hopefully)
--- src/components/parts/StickyProjectDisplay.tsx
+++ src/components/parts/StickyProjectDisplay.tsx
@@ -30,9 +30,9 @@ const StickyProjectDisplay = ({
     offset: ["start end", "end start"],
   });
 
-  let y20 = useTransform(scrollYProgress, [0, 1], ["0vh", "-20vh"]);
-  let y40 = useTransform(scrollYProgress, [0, 1], ["0vh", "-40vh"]);
-  let y60 = useTransform(scrollYProgress, [0, 1], ["0vh", "-60vh"]);
+  let y20 = useTransform(scrollYProgress, [0, 1], ["0vh", "-10vw"]);
+  let y40 = useTransform(scrollYProgress, [0, 1], ["0vh", "-20vw"]);
+  let y60 = useTransform(scrollYProgress, [0, 1], ["0vh", "30vw"]);
 
   useMotionValueEvent(scrollYProgress, "change", (latest) => {
     console.log("Page scroll: ", latest);
@@ -40,7 +40,7 @@ const StickyProjectDisplay = ({
 
   return (
     <div ref={scrollRef}>
-      <div className="w-screen h-screen max-w-full sticky top-0 flex flex-col justify-center items-center px-12 pb-12 pt-32 overflow-x-hidden">
+      <div className="w-screen h-screen max-w-full flex flex-col justify-center items-center px-12 pb-12 pt-32 overflow-x-hidden">
         <div className="flex flex-col items-center mb-10">
           <h1 className="text-6xl lg:text-8xl font-bold">{title}</h1>
           <p className=" max-w-sm text-center">{description}</p>
@@ -63,30 +63,27 @@ const StickyProjectDisplay = ({
           ></img>
         </div>
 
-        <div className="grid grid-cols-6 grid-rows-3 w-full h-full absolute top-0 gap-8 items-center justify-center -z-10 select-none">
+        <div className="grid grid-cols-6 grid-rows-3 w-full h-full absolute gap-8 items-center justify-center -z-10 select-none overflow-hidden">
           <motion.div
-            className="row-start-1 row-span-1 col-start-4 col-span-3 font-bold whitespace-nowrap opacity-10"
+            className="row-start-1 row-span-1 col-start-4 col-span-3 font-bold whitespace-nowrap opacity-10 text-zinc-600"
             style={{ fontSize: "30vh", translateX: y20 }}
           >
             {title}
           </motion.div>
           <motion.div
-            className=" row-start-2 row-span-1 col-start-1 col-span-3 font-bold whitespace-nowrap opacity-30"
+            className=" row-start-2 row-span-1 col-start-1 col-span-3 font-bold whitespace-nowrap opacity-30 text-zinc-600"
             style={{ fontSize: "10vh", translateX: y60 }}
           >
             {title}
           </motion.div>
           <motion.div
-            className=" row-start-3 row-span-1 col-start-3 col-span-3 font-bold whitespace-nowrap opacity-20"
+            className=" row-start-3 row-span-1 col-start-3 col-span-3 font-bold whitespace-nowrap opacity-20 text-zinc-600"
             style={{ fontSize: "20vh", translateX: y40 }}
           >
             {title}
           </motion.div>
         </div>
       </div>
-
-      {/* This is just for space to scroll on */}
-      <div className="w-screen h-screen max-w-full"></div>
     </div>
   );
 };
