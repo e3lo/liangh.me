e99cb5bfcf35e8c19d37bb6007b053ce38772768
Leo Heng <leoheng905@gmail.com>
2023-12-12 13:08:12 +1100

    Added delay to sushi train movement

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 import SkillIcon from "./parts/SkillIcon";
 
 import conveyorBelt from "../assets/SushiTrain/belt.png";
@@ -14,11 +16,40 @@ const SkillsSection = () => {
       <div className="w-1/2 md:w-2/3 flex flex-col justify-end pl-8 overflow-hidden">
         <div className="h-[500px] w-[1000px] self-start 2xl:self-end overflow-hidden relative">
           <img
+            draggable="false"
             src={conveyorBelt}
             className="h-[500px] w-[1000px] max-w-none object-cover object-left"
           ></img>
           <div className="absolute">
-            <SkillIcon title="Hi" body="Hello" iconPath="no" />
+            <SkillIcon title="VUE.JS" body="Hello" iconPath="no" delay={0} />
+            <SkillIcon title="React" body="Hello" iconPath="no" delay={335} />
+            <SkillIcon
+              title="TailwindCSS"
+              body="Hello"
+              iconPath="no"
+              delay={670}
+            />
+            <SkillIcon
+              title="Typescript"
+              body="Hello"
+              iconPath="no"
+              delay={1004}
+            />
+            <SkillIcon title="Scrum" body="Hello" iconPath="no" delay={1339} />
+            <SkillIcon title="Figma" body="Hello" iconPath="no" delay={1674} />
+            <SkillIcon
+              title="Framer Motion"
+              body="Hello"
+              iconPath="no"
+              delay={2009}
+            />
+            <SkillIcon
+              title="Version Control"
+              body="Hello"
+              iconPath="no"
+              delay={2343}
+            />
+            <SkillIcon title="Github" body="Hello" iconPath="no" delay={2678} />
           </div>
         </div>
       </div>
--- src/components/parts/SkillIcon.tsx
+++ src/components/parts/SkillIcon.tsx
@@ -1,30 +1,51 @@
-import { useRef } from "react";
-import { useAnimationFrame } from "framer-motion";
+import { useEffect, useRef } from "react";
+import { motion, useAnimationFrame } from "framer-motion";
 
 interface Props {
   title: string;
   body: string;
   iconPath: string;
+  delay: number;
 }
 
-const SkillIcon = ({}: Props) => {
+function initialPos(delay: number) {
+  let x = 1000;
+  let y = -139;
+
+  if (delay < 989) {
+    x -= delay;
+    y = -139;
+  } else if (delay < 1339) {
+    x = 11;
+    y -= delay - 989;
+  } else if (delay < 2328) {
+    y = -489;
+    x = 11 + (delay - 1339);
+  } else {
+    y = -489 + (delay - 2328);
+    x = 1000;
+  }
+
+  return [x, y];
+}
+
+const SkillIcon = ({ title, delay }: Props) => {
   const ref = useRef<HTMLDivElement>(null);
 
-  let x = 11;
-  let y = -139;
+  let [x, y] = initialPos(delay);
 
   useAnimationFrame((time, delta) => {
     if (ref.current != null) {
-      if (x == 11) {
-        y--;
-      } else if (x == 1000) {
-        y++;
-      }
-
       if (y == -139) {
         x--;
       } else if (y == -489) {
         x++;
+      }
+
+      if (x == 11) {
+        y--;
+      } else if (x == 1000) {
+        y++;
       }
 
       ref.current.style.transform = `translate(${x}px, ${y}px)`;
@@ -33,10 +54,10 @@ const SkillIcon = ({}: Props) => {
 
   return (
     <div
-      className=" bg-zinc-200 w-32 h-32 rounded-full flex items-center justify-center absolute"
+      className=" bg-zinc-200 w-32 h-32 rounded-full flex items-center justify-center absolute text-neutral-950"
       ref={ref}
     >
-      🚩
+      {title}
     </div>
   );
 };
