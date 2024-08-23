a7972414f5d83fdb6d405da37769b9231b4adf8b
Leo Heng <leoheng905@gmail.com>
2024-08-23 23:53:10 +1000

    Updated navbar visibility

--- src/components/parts/NavigationBar.tsx
+++ src/components/parts/NavigationBar.tsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom'
 
 const NavigationBar = () => {
   return (
-    <nav className="fixed w-screen max-w-full flex justify-center p-4 bg-neutral-50/10 backdrop-blur-md border-b-2 border-b-white">
+    <nav className="fixed w-screen max-w-full flex justify-center p-4 bg-neutral-50/70 backdrop-blur-lg border-b-2 border-b-white">
       <Link to={'/'} className=" font-display text-4xl">
         <h1 className=" hover:scale-105 hover:text-orange-500 transition-all">
           LIANG HENG
